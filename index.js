2407c9a94f5e0c1e06aa73d2f60b28bdb2261462
Mark Helbling <mdhelbling@gmail.com>
2022-12-09 12:36:54 -0600

    commit tree 8de00c9af4fed0c126ee7437538eafeb91a70685

---
+++ index.js
@@ -0,0 +1,44 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide () {
+
+}
+
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function increment(a, n) {
+    return a + 1;
+}
+
+function decrement(a, n) {
+    return a - 1;
+}
+
+function makeInt(n) {
+    console.log (makeInt((n, 10));
+    return (n);
+}
